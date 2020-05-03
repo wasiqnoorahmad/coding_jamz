7aa3b3b19424b15d3ae3bf7efe8efa396a3ddbe9
Wasiq Noor <wasiqnoorahmad@gmail.com>
2020-05-03 21:33:14 +0500

    Remove duplicates from sorted array

---
+++ remove_dup_sorted.cpp
@@ -0,0 +1,30 @@
+/* This program removes duplicates from a sorted array. Statement can be found
+ * at:
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+ */
+
+#include <bits/stdc++.h>
+
+#include "common.hpp"
+using namespace std;
+
+int rmv_duplicates(vector<int>& nums) {
+  int left = 0, right = 1;
+
+  while (left < nums.size()) {
+    right = left + 1;
+    while (right < nums.size() && nums[left] == nums[right]) right++;
+    nums.erase(nums.begin() + left + 1, nums.begin() + right);
+    left++;
+  }
+  return nums.size();
+}
+
+int main(int argc, char const* argv[]) {
+  vector<int> nums = {0};
+  p_vector(nums);
+  int size = rmv_duplicates(nums);
+  printf("New size is %d\n", size);
+  p_vector(nums);
+  return 0;
+}
--- word_ladder.cpp
+++ word_ladder.cpp
@@ -4,55 +4,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int hamming_dist(string &src, string &dst) {
+const bool is_reachable(string src, string dst) {
   int walk = 0, diff = 0;
-
-  while (walk < src.length()) {
+  while (walk != src.length()) {
     if (src[walk] != dst[walk]) diff++;
     walk++;
   }
-  return diff;
+  return diff == 1;
 }
 
 int solve(string src, string target, vector<string> &dict) {
-  int steps = 1, min_diff = INT_MAX, diff;
-  string tmp = src, min_str;
-  map<string, bool> selected;
+  if (src == target) return 1;
+  if (is_reachable(src, target)) return 2;
 
-  while (src != target) {
-    if (hamming_dist(src, target) == 1) return steps + 1;
+  /* Here goes the BFS */
+  vector<bool> visited(dict.size(), false);
+  queue<pair<int, string>> track;
+  track.push(make_pair(1, src));
 
-    min_diff = INT_MAX;
+  while (!track.empty()) {
+    auto curr = track.front();
+    track.pop();
 
-    for (string &word : dict) {
-      if (word == src) continue;
+    if (is_reachable(curr.second, target)) return curr.first + 1;
 
-      if (hamming_dist(word, src) == 1) {
-        diff = hamming_dist(word, target);
-
-        if (diff < min_diff) {
-          min_diff = diff;
-          min_str = word;
-        }
+    for (int walk = 0; walk < dict.size(); walk++) {
+      if (!visited[walk] && is_reachable(curr.second, dict[walk])) {
+        visited[walk] = true;
+        track.push(make_pair(curr.first + 1, dict[walk]));
       }
     }
-
-    if (src == min_str) return 0;
-
-    src = min_str;
-    steps++;
   }
 
-  return steps;
+  return 0;
 }
 
 int main(int argc, char const *argv[]) {
   vector<string> dict = {"hot", "dot", "dog", "lot", "log"};
   printf("%d\n", solve("hit", "cog", dict));
 
-  /* A : "ymain"
-        B : "oecij"
-        C : [ "ymann", "yycrj", "oecij", "ymcnj", "yzcrj", "yycij", "xecij",
-     "yecij", "ymanj", "yzcnj", "ymain" ]  10 */
+  // vector<string> dict = {"ymann", "yycrj", "oecij", "ymcnj", "yzcrj",
+  // "yycij",
+  //                        "xecij", "yecij", "ymanj", "yzcnj", "ymain"};
+  // printf("%d\n", solve("ymain", "oecij", dict));  // 10
+
+  // vector<string> dict = {
+  //     "mzqwkyfqdfkkhdtrwmxm", "mlqdryzxdwukhdtpqqkv", "pyudlolywuuiexfdqevw",
+  //     "mzqwrylbdfkkhdtfbqxm", "yyudloxxvwumevoyqqyw", "yyqdfozevwumedoyqqyk",
+  //     "nzqwkyfqdfkkhdtywmkm", "yyqdfozevwumedowqqyv", "uhoddciyreuihqftqvjl",
+  //     "mzqwryzbdfkkhdtfbqxm", "yyudfozxvwumevoyqqyk", "pyndlciyuuuieqftqvjl",
+  //     "mzqdryzxdwkkhdtpqqxm", "ylqduozxvwumedowqqyv", "mzqwkyfqdfkkhdtfwqxm",
+  //     "pyndlciyuuuieqfhqvjl", "nlqduyzxvwumedopqqqv", "pyudlolywuuiexfhqevw",
+  //     "uhojdciyrnughwfiqvil", "mzqwkyfqdfkkhdtrwqxm", "yyqdfozevwumevoyqqyk",
+  //     "pyodlciyuruiyqftqvjl", "pyudlciyuuuiexfhqevw", "mzqwrylqdfkkhdtfbqxm",
+  //     "nlqdryzxdwukhdtpqqkv", "uhoddciyreughwftqvil", "nlqduozxvwumedowqqyv",
+  //     "pyudlclywuuiexfhqevw", "nlqduyzxdwukedopqqqv", "hzqxcydbdxkkcdtzwgkm",
+  //     "pyudlciyuuuieffhqevw", "pyudlolyvwuiexodqevw", "uhoddciyreughwftqvjl",
+  //     "nzqwcyfbdxkkhdtzwmkm", "pyodlciyuuuiyqftqvjl", "nlqduyzxdwukhdtpqqkv",
+  //     "fyudloxyvwumevoyqqyw", "uhojdciyreughwfiqvil", "mzqwkyfqdfkkhdtfbqxm",
+  //     "nzqwcyfqdfkkhdtzwmkm", "pyudlciyuuuiebfhqvjl", "pyoddciyaeuihqftqvjl",
+  //     "yyqdfozpvwumedowqqyv", "nzqwcydbdxkkhdtzwmkm", "mzqwryzbdwkkhdtpbqxm",
+  //     "pyudloxyvwuievodqeyw", "fyudloxyvwumevodqqyw", "pyodlciyuuuieqftqvjl",
+  //     "yyudfozevwumevoyqqyk", "yyqdfozevwumedowqqyk", "hzqwcydbdxkkcdtzwgkm",
+  //     "mzqwkyfqdfkkhdtywmxm", "nzqwcydbdxkkcdtzwgkm", "nlqdubzxvwumedopqqyv",
+  //     "pyudlolyvuuiexfdqevw", "yyudloxyvwumevoyqqyw", "yyudlozxvwumevoyqqyw",
+  //     "pyudlciyuuuiebfhqvjw", "mzqwkylqdfkkhdtfbqxm", "mkqdryzxdwukhdtpqqkv",
+  //     "hzqxcydbdxkbcdtzwgkm", "nzqwqyfqdfkkhdtzwmkm", "mzqwryzzdwkkhdtpqqxm",
+  //     "fyudloxyvwumevodqeyw", "mzqdryzzdwkkhdtpqqxm", "pyudlciyuuuiebfhqevw",
+  //     "pyodlciyueuiyqftqvjl", "nlqduyzxvwukedopqqqv", "nlqdubzxvwumedowqqyv",
+  //     "mzqwkyfqdfkkhdtywmkm", "phoddciyreuihqftqvjl", "ylqduozpvwumedowqqyv",
+  //     "pyoddciyaeuiyqftqvjl", "nlqduyzxdwukhdtpqqqv", "uhoddciyreuihwftqvjl",
+  //     "yyudlozxvwumevoyqqyk", "nlqduyzxdwukhdopqqqv", "pyudlciywuuiexfhqevw",
+  //     "pyudloxyvwuiexodqeyw", "mzqwryzbdfkkhdtpbqxm", "nzqwkyfqdfkkhdtzwmkm",
+  //     "nzqwcyfbdfkkhdtzwmkm", "meqdryzxdwukhdtpqqkm", "mzqwryzbdwkkhdtpqqxm",
+  //     "nzqwcydbdxkkcdtzwmkm", "pyudloxyvwuiexodqevw", "fyudloxyvwuievodqeyw",
+  //     "pyudlolyvuuiexodqevw", "mzqdryzxdwukhdtpqqxm", "pyudlciyuuuieqfhqvjl",
+  //     "meqdryzxdwukhdtpqqkv", "uhoddciyreughwfiqvil", "pyodlciyaeuiyqftqvjl",
+  //     "nlqdubzxvwumedopqqqv", "meqdryzxdwukhdtpqqxm", "yyqduozpvwumedowqqyv",
+  //     "pyudlciyuuuiebfhqejw", "phoddciyaeuihqftqvjl"};
+  // printf("%d\n",
+  //        solve("hzqxcydbdxkbcdtzwgkm", "uhojdciyrnughwfiqvil", dict));  // 94
+
   return 0;
 }
