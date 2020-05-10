ffe7c851cf6ff993ce935f219c759ec94f320a44
Wasiq Noor <wasiqnoorahmad@gmail.com>
2020-05-11 01:17:24 +0500

    Adding subset sum partition problem with bool array

--- subset_sum.cpp
+++ subset_sum.cpp
@@ -12,31 +12,30 @@ bool partition(vector<int> &nums) {
 
   total /= 2;
 
-  int dp[nums.size() + 1][total + 1];
+  bool dp[nums.size() + 1][total + 1];
 
   /* If there is zero item, no sum can be achieved */
-  for (int t = 0; t <= total; t++) dp[0][t] = 0;
+  for (int t = 0; t <= total; t++) dp[0][t] = false;
   /* If total sum is zero, it can be always achieved with empty set */
-  for (int i = 0; i <= nums.size(); i++) dp[i][0] = 0;
+  for (int i = 0; i <= nums.size(); i++) dp[i][0] = true;
 
   /* Build up till all sums */
   for (int i = 1; i <= nums.size(); i++) {
     for (int t = 1; t <= total; t++) {
-      if (t < nums[i - 1])
-        dp[i][t] = dp[i - 1][t];
-      else
-        dp[i][t] = max(dp[i - 1][t], nums[i - 1] + dp[i - 1][t - nums[i - 1]]);
+      dp[i][t] = dp[i - 1][t];
+      if (t >= nums[i - 1])
+        dp[i][t] = dp[i - 1][t] || dp[i - 1][t - nums[i - 1]];
     }
   }
 
-  for (int i = 1; i <= nums.size(); i++)
-    if (dp[i][total] == total) return true;
-  return false;
+  return dp[nums.size()][total];
 }
 
 int main(int argc, char const *argv[]) {
-  vector<int> nums = {478, 757, 314, 471, 729, 100, 459, 618};
+  // vector<int> nums = {478, 757, 314, 471, 729, 100, 459, 618};
   // vector<int> nums = {1, 5, 11, 5};
+  vector<int> nums = {3, 1, 1, 2, 2, 1};
+
   if (partition(nums))
     printf("YES\n");
   else
