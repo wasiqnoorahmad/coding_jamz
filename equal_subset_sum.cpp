8f2f5687d223c56437fa5d5c494cf25c20582b70
Wasiq Noor <wasiqnoorahmad@gmail.com>
2020-07-15 20:49:04 +0500

    Adding Equal Subset Partition, and Subset Sum

--- equal_subset_sum.cpp
+++ equal_subset_sum.cpp
@@ -1,11 +1,24 @@
-/* This program checks if there exists a subset of sum k */
+/* This program solves the equal subset sum problem given at:
+ * https://leetcode.com/problems/partition-equal-subset-sum/ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
-bool is_subset_sum(vector<int> &nums, int target) {
-  vector<vector<bool>> dp(nums.size() + 1, vector<bool>(target + 1, false));
+bool can_partition(vector<int> &nums) {
+  int sum = 0;
+  for (int n : nums)
+    sum += n;
+
+  if (sum % 2 == 1)
+    return false;
+
+  sum /= 2;
+
+  // row = nums, col = sum
+  vector<vector<bool>> dp(nums.size() + 1, vector<bool>(sum + 1, false));
+
   for (int i = 0; i <= nums.size(); i++) {
-    for (int j = 0; j <= target; j++) {
+    for (int j = 0; j <= sum; j++) {
       if (i == 0 && j == 0)
         dp[i][j] = true;
       else if (i == 0)
@@ -18,12 +31,12 @@ bool is_subset_sum(vector<int> &nums, int target) {
         dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
     }
   }
-  return dp[nums.size()][target];
+  return dp[nums.size()][sum];
 }
 
 int main(int argc, char const *argv[]) {
   vector<int> nums = {1, 5, 11, 5};
-  if (is_subset_sum(nums, 6))
+  if (can_partition(nums))
     printf("True\n");
   else
     printf("False\n");
